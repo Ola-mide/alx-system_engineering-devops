be2139c9748940dff4c3b0d114ea2bb3953e6384
Abdulrahman <abdulrahmanoyetade@gmail.com>
2022-11-15 15:57:30 +0100

    a ruby script that accepts one argument and passes it to a regular expression matching method

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+sender = ARGV[0].scan(/from:[0-9A-Za-z\-+]+/).join
+receiver = ARGV[0].scan(/to:[0-9A-Za-z\-+]+/).join
+flags = ARGV[0].scan(/flags:[0-9A-Za-z\-+:]+/).join
+
+sender["from:"] = ""
+receiver["to:"] = ""
+flags["flags:"] = ""
+
+puts "#{sender},#{receiver},#{flags}"
